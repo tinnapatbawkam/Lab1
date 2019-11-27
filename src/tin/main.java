c45c2c802afd4acca231eea399edb6458211b1ae
tinnapatbawkam <tinnapatbawkam@gmail.com>
2019-11-27 23:41:21 +0700

    Commit1

---
+++ src/tin/main.java
@@ -0,0 +1,33 @@
+package tin;
+
+public class main {
+
+	public static void main(String[] args) {
+		int a, b, c = 2;
+		double gpa = 3.50;
+		String firstName = "Tinnapat Bokham";
+		
+		System.out.println("Hello My Name is " + firstName);
+		
+		for(int i=0; i<10; i++)
+			if(i % 2 == 0)
+			System.out.println("Hello, " + i);
+			//System.out.print(c);
+		
+		int[] numbers = {1, 1, 2, 3, 5};
+		int[] even = new int[20];
+		String[] fruits = {"banana", "apple", "melon", "orange"};
+		
+		//for(int i=0; i<fruits.length; i++)
+		//	System.out.println(fruits[i]);
+		//for-each loop
+		//for(String f : fruits)
+		//	System.out.println(f);
+
+		String tel = "053-211-122";
+		String[] sp = tel.split("-");
+		System.out.println("("+sp[0]+")"+sp[1]);
+
+	}
+
+}
